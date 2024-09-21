a58125f2ce6095f38b5a4b9073cca46a90410db2
Haroon <rajaharoon973@gmail.com>
2024-09-21 19:02:09 +0500

    bloc_counter_example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:bloc_flutter/UI/counter_screen.dart';
+import 'package:bloc_flutter/bloc/counter/counter_bloc.dart';
 import 'package:bloc_flutter/equatable_testing.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(create: (_)=>CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home:CounterScreen(),
       ),
-      home:const EquatableTesting(),
     );
   }
 }
